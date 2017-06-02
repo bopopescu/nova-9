15e5561489d7a7da18ee7f6801289af4c806efa0
lidongbj <lidongbj@inspur.com>
2017-06-02 16:40:16 -0400

    update mount , unmount

--- nova/api/openstack/compute/ics_vm.py
+++ nova/api/openstack/compute/ics_vm.py
@@ -72,7 +72,19 @@ class IcsVmController(wsgi.Controller):
         self._validate_vm(context, vmid)
         # do ics-vm mount iso
         LOG.info("begin to mount iso to ics_vm")
-        res = {'success': True}
+        try:
+            task_info = self._ics_manager.vm.attach_cdrom(vm_id=vmid, isoid=isoid)
+        except Exception as e:
+            # do something
+            LOG.error("mount iso to ics_vm exception : " + e.message)
+            pass
+        LOG.info("end to mount iso to ics_vm")
+        state = task_info['state']
+        if state == 'FINISHED':
+            res = {'success': True}
+        else:
+            res = {'success': False}
+
         return dict(vmMount=res)
 
     @extensions.expected_errors(404)
@@ -84,7 +96,18 @@ class IcsVmController(wsgi.Controller):
         self._validate_vm(context, vmid)
         # do ics-vm unmount iso
         LOG.info("begin to unmount iso to ics_vm")
-        res = {'success': True}
+        try:
+            task_info = self._ics_manager.vm.detach_cdrom(vm_id=vmid)
+        except Exception as e:
+            # do something
+            LOG.error("unmount iso to ics_vm exception : " + e.message)
+            pass
+        LOG.info("end to unmount iso to ics_vm")
+        state = task_info['state']
+        if state == 'FINISHED':
+            res = {'success': True}
+        else:
+            res = {'success': False}
         return dict(vmUnmount=res)
 
     def _validate_image(self, context, isoid):
