1a553c17561c02cd79cb300e97cdd1840cff6006
sunjia <sunjia@inspur.com>
2017-06-02 18:52:21 +0800

    fix bug

--- nova/api/openstack/compute/servers.py
+++ nova/api/openstack/compute/servers.py
@@ -551,7 +551,7 @@ class ServersController(wsgi.Controller):
         create_kwargs = {}
 
         # ics_node for buiid the instance
-        if body.haskey('ics_node'):
+        if body.has_key('ics_node'):
             ics_node = body['ics_node']
         else:
             ics_node = None
